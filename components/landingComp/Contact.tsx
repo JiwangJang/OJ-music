ef79037b77d9bae1e84f721b07ca64f8a4060240
dkalek1 <jiwang0917@naver.com>
2024-04-07 19:44:55 +0900

    랜딩페이지 문구위치 조정 및 상담문의페이지 내용수정

--- components/landingComp/Contact.tsx
+++ components/landingComp/Contact.tsx
@@ -12,36 +12,25 @@ export default function Contact() {
     }, []);
 
     return (
-        <div className="container division-padding">
+        <div className='container division-padding'>
             <div className={style.contactContainer}>
-                <p className={`${style.hookingment} head-1`}>
-                    {isMobile ? (
-                        <>
-                            지금 바로 당신의 꿈을 <br />
-                            펼쳐보세요
-                        </>
-                    ) : (
-                        <>
-                            지금 바로 <br />
-                            당신의 꿈을
-                            <br /> 펼쳐보세요
-                        </>
-                    )}
+                <p className={`${style.hookingment} head-1`} style={{ whiteSpace: "pre" }}>
+                    {isMobile ? `지금 바로 당신의 꿈을\n펼쳐보세요` : `지금 바로\n당신의 꿈을\n펼쳐보세요`}
                 </p>
                 <div className={style.contactForm}>
                     <div>
-                        <label htmlFor="phone-number" className="head-4">
+                        <label htmlFor='phone-number' className='head-4'>
                             연락처
                         </label>
-                        <input type="tel" id="phone-number" />
+                        <input type='tel' id='phone-number' />
                     </div>
                     <div>
-                        <label htmlFor="query" className="head-4">
+                        <label htmlFor='query' className='head-4'>
                             문의사항
                         </label>
-                        <input type="text" id="query" />
+                        <input type='text' id='query' />
                     </div>
-                    <button className="head-4">상담신청</button>
+                    <button className='head-4'>상담신청</button>
                 </div>
             </div>
         </div>
