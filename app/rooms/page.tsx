faa4090d7bbc09c52b95241a4a65df14177aa9f0
Jang Jiwang <jiwang0917@naver.com>
2024-04-08 17:17:29 +0900

    시설안내 사진 만들기

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -1,3 +1,12 @@
+import Intro from "@/components/layoutComp/Intro";
+import RoomsPhotos from "@/components/roomsComp/RoomsPhotos";
+import introImage from "@/public/image/rooms/landing.jpg";
+
 export default function Page() {
-  return <div>시설안내페이지임</div>;
+    return (
+        <div>
+            <Intro imgSrc={introImage} title="시설안내" subTitle="오제이 음악학원의 우수한 시설을 만나보세요" />
+            <RoomsPhotos />
+        </div>
+    );
 }
